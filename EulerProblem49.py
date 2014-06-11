f5f206eb87e5aeec8b225bd6a2821a1ec7533094
shpetz <surya.das@hotmail.com>
2014-06-11 21:26:54 +1000

    First commit. Almost finished, just need to concat the numbers to meet the requirements of the problem.

---
+++ EulerProblem49.py
@@ -0,0 +1,67 @@
+#!/usr/local/bin/python2.7
+  
+def main():
+  primeList = []
+  primePermList = []
+  for x in range(1000,10000):
+    if checkPrime(x) == True:
+      primeList.append(x)    
+      #print "prime!" + str(x)
+  for prime in primeList:
+    #print prime
+    checkSequence(permutate(prime))
+  
+def checkSequence(primeList):  
+  culledPrimeList = list(set(primeList))
+  #print culledPrimeList
+  #convert strings to int
+  culledPrimeList = map(int, culledPrimeList)
+  
+  #filter out the non prime permutations
+  for prime in culledPrimeList:
+    if not checkPrime(prime):
+      culledPrimeList.remove(prime)
+  
+  diffA = 0
+  diffB = 0
+  temp = culledPrimeList[0]
+  
+  for x in range(1, len(culledPrimeList)):
+    diffA = abs(culledPrimeList[x] - temp)
+    
+    #out of bounds check
+    if culledPrimeList[x] == culledPrimeList[-1]:
+      diffB = 0
+    else:
+      diffB = abs(culledPrimeList[x] - culledPrimeList[x+1])
+    
+    if diffA == diffB:
+      print 'WE HAVE A MATCH!: ' + str(temp) + " " + str(culledPrimeList[x]) + " " + str(culledPrimeList[x+1])
+      
+    temp = culledPrimeList[x]
+    
+def checkPrime(n):
+  i = 2
+  while i < n:
+    if n % i == 0:
+      return False
+    else:
+      i += 1
+  return True  
+  
+def permutate(seq):
+  seq = str(seq) #only works with strings
+  if not seq:
+    return [seq] # is an empty sequence
+  else:
+    temp = []
+  for k in range(len(seq)):
+    part = seq[:k] + seq[k+1:]
+    #print k, part # test
+    for m in permutate(part):
+      temp.append(seq[k:k+1] + m)
+      #print m, seq[k:k+1], temp # test
+  return temp
+
+if __name__ == "__main__":
+  main()

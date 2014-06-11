e2bf3b4045c6fb7794da5293bbcecca1155f27d0
Shpetz <surya.das@hotmail.com>
2014-06-11 22:00:01 +1000

    Added problem description, made output nicer looking

--- Problem49/EulerProblem49.py
+++ Problem49/EulerProblem49.py
@@ -1,5 +1,17 @@
 #!/usr/local/bin/python2.7
   
+"""
+The arithmetic sequence, 1487, 4817, 8147, in which each
+of the terms increases by 3330, is unusual in two ways:
+(i) each of the three terms are prime, and,
+(ii) each of the 4-digit numbers are permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+exhibiting this property, but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this sequence?
+"""
+
 def main():
   primeList = []
   primePermList = []
@@ -36,8 +48,10 @@ def checkSequence(primeList):
       diffB = abs(culledPrimeList[x] - culledPrimeList[x+1])
     
     if diffA == diffB:
-      print 'WE HAVE A MATCH!: ' + str(temp) + " " + str(culledPrimeList[x]) + " " + str(culledPrimeList[x+1])
-      
+      series = [temp, culledPrimeList[x], culledPrimeList[x+1]]
+      print "Series:"
+      print sorted(series)
+            
     temp = culledPrimeList[x]
     
 def checkPrime(n):
